1102fee1babee9f20077078a0d2fd0f283ad7a85
Dawid <dawmic19@gmail.com>
2023-03-28 21:09:06 +0200

    Add html boilerplate and favicon files

---
+++ app.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/skills">Skills</NuxtLink></li>
+          <li><NuxtLink to="/work">Work</NuxtLink></li>
+          <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <NuxtPage />
+    </main>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  .router-link-exact-active{
+      color: green;
+      font-size: 2rem;
+      border: 2px solid black;
+      padding: 1rem;
+  }
+ 
+  
+  </style>
+  
